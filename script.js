f6a9489d59e03376cb5b2aad19a34ab52ba961f0
JosephCarmeli <josephcarmeli@hotmail.com>
2023-04-05 15:33:25 -0500

    complete

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ for (let i = 0; i < 8; i++) {
     tile.style.float = "left";
     container.appendChild(tile);
 
-    hello
     const red = Math.floor(Math.random() * 256);
     const green = Math.floor(Math.random() * 256);
     const blue = Math.floor(Math.random() * 256);
